c9fb08daed2e2df262b19ae2128d0a2da8131f1e
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2020-10-28 09:59:26 -0400

    Unsuccessful first attempt at removing HTML email email element and adding back with JS

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 document.querySelector('#myButton').addEventListener('click', function() {
     document.querySelector('#message').innerText = "La Croix, America's favorite spicy water drink"
 });
+
+
+// const divElement = document.createElement('div');
+// divElement.classList('form-group');
+
+// const emailLabel = document.createElement('label');
+// emailLabel.textContent = 'Email';
+// emailLabel.setAttribute('for', 'inputEmail');
+
+// form.divElement.appendChild(emailLabel);
